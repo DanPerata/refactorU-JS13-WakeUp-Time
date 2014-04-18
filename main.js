73b4105daa8250b08d9e77dd31f1b562ac2dc513
Dan Perata <djperata@aol.com>
2014-04-18 15:36:28 -0600

    Added mulitiple divs to give appearance of depth. Added static radio dial.

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 $(document).on('ready', function() {
-  var outerShell= $('<div class= "outerShell"></div>');
-  var innerShell= $('<div class= "innerShell"></div>');
+  var outerShell= $('<div class= "outerShell shellAppearance"></div>');
+  var outerShell1= $('<div class= "outerShell shellAppearance" id= "outerShell1"></div>');
+  var outerShell2= $('<div class= "outerShell shellAppearance" id= "outerShell2"></div>');
+  var outerShell3= $('<div class= "outerShell shellAppearance" id= "outerShell3"></div>');
+  var outerShell4= $('<div class= "outerShell shellAppearance" id= "outerShell4"></div>');
+  var outerShell5= $('<div class= "outerShell shellAppearance" id= "outerShell5"></div>');
+
+  var innerShell= $('<div class= "innerShell shellAppearance"></div>');
   var pmLabel= $('<div class= "pmLabel">PM</div>');
   var clockScreen= $('<div class= "clockScreen"></div>');
   var indicator= $('<div class= "indicator">*</div>');
   var clockText= $('<div class= "clockText">clockText</div>');
-  var amLabel= $('<div class= "amLabel">AM                              53 60 70 90 110 140 170</div>');
-  var fmLabel= $('<div class= "fmLabel">FM                              88 92 96 102 106 108 </div>');
+  var amLabel= $('<div class= "amLabel">AM             53       60       70       90       110       140       170</div>');
+  var fmLabel= $('<div class= "fmLabel">FM             92       96       102       104        106          108 </div>');
+  var slider= $('<div class= "slider">Slider</div>');
+  var sliderIndicator= $('<div class= "sliderIndicator"></div>');
   var newDate= new Date();
   console.log(newDate);
   console.log(newDate.toLocaleTimeString());
@@ -16,14 +24,21 @@ $(document).on('ready', function() {
 
 
 
-$('.container').append(outerShell);
+$('.container').append(outerShell5);
+
+$(outerShell5).append(outerShell4);
+$(outerShell4).append(outerShell3);
+$(outerShell3).append(outerShell2);
+$(outerShell2).append(outerShell1);
+$(outerShell1).append(outerShell);
+
 $(outerShell).append(innerShell);
-
-
 
 $(innerShell).append(pmLabel);
 $(innerShell).append(amLabel);
 $(innerShell).append(fmLabel);
+$(innerShell).append(slider);
+$(slider).append(sliderIndicator);
 
 $(innerShell).append(clockScreen);
 $(clockScreen).append(indicator);
