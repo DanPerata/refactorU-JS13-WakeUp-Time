2c6e4721437905f7fce6d02c0edf75cf80578764
Dan Perata <djperata@aol.com>
2014-04-19 11:59:38 -0600

    added indicator of scroll value

--- main.js
+++ main.js
@@ -13,9 +13,11 @@ $(document).on('ready', function() {
   var clockText= $('<div class= "clockText">clockText</div>');
   var amLabel= $('<div class= "amLabel">AM             53       60       70       90       110       140       170</div>');
   var fmLabel= $('<div class= "fmLabel">FM             92       96       102       104        106          108 </div>');
-  var slider= $('<div class= "slider">Slider</div>');
+  var slider= $('<div class= "slider">slider</div>');
   var sliderIndicator= $('<div class= "sliderIndicator"></div>');
+  var dial= $('<div id="slider"></div>');
   var newDate= new Date();
+  var station= $('<div class= "station"></div>')
 
   
   var myClock=setInterval(function(){myTimer()},1000);
@@ -23,12 +25,22 @@ $(document).on('ready', function() {
   function myTimer()
   {
   var d=new Date();
-  if(d.getHours() <12){
-    $(indicator).css('display', 'none');
+  var hours= d.getHours();
+  var minutes= d.getMinutes();
+  if(hours <12){
+    $(indicator).css('visibility', 'hidden');
+  }
+  else{
+    $(indicator).css("visibility", 'visible');
+    hours= hours-12;
+  }
+
+  if(minutes <10){
+    minutes= "0" + minutes;
   }
 
 
-  var t=d.getHours() + ':' + d.getMinutes();
+  var t= hours + ':' + minutes;
   $(clockText).text(t);
 
 
@@ -59,6 +71,18 @@ $(innerShell).append(slider);
 $(slider).append(sliderIndicator);
 
 $(innerShell).append(clockScreen);
+$(innerShell).append(dial);
+$(innerShell).append(station);
+$(dial).slider({ 
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( station ).text( ui.value );
+      }});
+   // $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+   //  });
 $(clockScreen).append(indicator);
 $(clockScreen).append(clockText);
 
